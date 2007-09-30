8f858d6c6402c05170fe96ef03f367dbba3a1fc7
xtang@qnx.com <xtang@qnx.com@b0d920d6-249f-4265-941a-613bfc6fc4a4>
2007-09-30 03:04:38 +0000

    Also include <strings.h> as strcasecmp/strncasecmp is defined there.

--- HEAD/pkgsrc/pkgtools/pbulk/files/pbulk/lib/match.c
+++ HEAD/pkgsrc/pkgtools/pbulk/files/pbulk/lib/match.c
@@ -38,6 +38,10 @@
 #include <nbcompat/stdio.h>
 #include <nbcompat/string.h>
 
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+
 #include "pbulk.h"
 
 static int dewey_cmp(const char *, int, const char *);
