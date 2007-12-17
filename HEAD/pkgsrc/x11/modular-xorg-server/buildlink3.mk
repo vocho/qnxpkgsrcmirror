fbd0837e5b157875da48d94f55cb4d0ed72057f3
christian.leutloff <christian.leutloff@xoxind.de>
2007-12-17 11:20:19 +0000

    excluded libdrm from the build process - changes should not harm upstream; added ucpp as a possible cpp to the configure script

--- HEAD/pkgsrc/x11/modular-xorg-server/buildlink3.mk
+++ HEAD/pkgsrc/x11/modular-xorg-server/buildlink3.mk
@@ -23,7 +23,9 @@ BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
 .include "../../x11/renderproto/buildlink3.mk"
 .include "../../x11/videoproto/buildlink3.mk"
 .include "../../x11/xextproto/buildlink3.mk"
+.if ${OPSYS} != "QNX"
 .include "../../x11/xf86driproto/buildlink3.mk"
+.endif
 .include "../../x11/xproto/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
