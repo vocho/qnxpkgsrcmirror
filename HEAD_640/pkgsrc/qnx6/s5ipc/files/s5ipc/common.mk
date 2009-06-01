25f15f6efab07334710ac765444c1ac5ade7162a
xtang@qnx.com <xtang@qnx.com@b0d920d6-249f-4265-941a-613bfc6fc4a4>
2009-06-01 19:05:31 +0000

    Some packages only useful on QNX6.

---
+++ HEAD_640/pkgsrc/qnx6/s5ipc/files/s5ipc/common.mk
@@ -0,0 +1,22 @@
+# This is an automatically generated record.
+# The area between QNX Internal Start and QNX Internal End is controlled by
+# the QNX IDE properties.
+
+
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+NAME=s5ipc
+
+PUBLIC_INCVPATH=	$(PROJECT_ROOT)/lib/public
+EXTRA_INCVPATH+=	$(PROJECT_ROOT)/manager/inc
+
+EXTRA_SILENT_VARIANTS=	$(SECTION)
+
+include $(MKFILES_ROOT)/qmacros.mk
+
+include $(SECTION_ROOT)/$(SECTION).mk
+
+include $(MKFILES_ROOT)/qtargets.mk
