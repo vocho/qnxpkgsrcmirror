c1053663c11425e6a7056aebacd4ef6a0645f02d
seanb <seanb@qnx.com>
2009-06-13 23:16:57 +0000

    - fixes.

--- HEAD_641/pkgsrc/devel/scmgit-base/files/qnxnto.c
+++ HEAD_641/pkgsrc/devel/scmgit-base/files/qnxnto.c
@@ -1,4 +1,5 @@
 #include "qnxnto.h"
+#undef sigaction
 
 #include <sys/neutrino.h>
 #include <pthread.h>
@@ -19,23 +20,28 @@ sigaction_qnx(int sig, const struct sigaction *act, struct sigaction *oact)
 	static pthread_t	restart_tid;
 	sigset_t		nset;
 	int			ret;
+	struct sigaction	act_new;
 
-	if (act->sa_flags & SA_RESTART) {
-		if (restart_tid == 0 && (ret = pthread_create(&restart_tid,
-		    NULL, restart_thread, NULL)) != EOK) {
-			errno = ret;
-			return -1;
-		}
-
-		/* Block in calling thread */
-		sigemptyset(&nset);
-		sigaddset(&nset, sig);
-		pthread_sigmask(SIG_BLOCK, &nset, NULL);
-
-		/* Make sure unblocked in restart thread if not already */
-		SignalProcmask_r(0, restart_tid, SIG_UNBLOCK, &nset, NULL);
-	}
-	else {
+	if ((act->sa_flags & SA_RESTART) == 0) {
 		return sigaction(sig, act, oact);
 	}
+
+	/* SA_RESTART */
+	if (restart_tid == 0 && (ret = pthread_create(&restart_tid,
+	    NULL, restart_thread, NULL)) != EOK) {
+		errno = ret;
+		return -1;
+	}
+
+	/* Block in calling thread */
+	sigemptyset(&nset);
+	sigaddset(&nset, sig);
+	pthread_sigmask(SIG_BLOCK, &nset, NULL);
+
+	/* Make sure unblocked in restart thread if not already */
+	SignalProcmask_r(0, restart_tid, SIG_UNBLOCK, &nset, NULL);
+
+	act_new = *act;
+	act_new.sa_flags &= ~SA_RESTART;
+	return sigaction(sig, &act_new, oact);
 }
