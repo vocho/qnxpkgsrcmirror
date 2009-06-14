5eb72fcacdb493e475ee474222eb42af64f8a9d1
seanb <seanb@qnx.com>
2009-06-14 14:20:15 +0000

    - cleanup.

--- HEAD_641/pkgsrc/devel/scmgit-base/files/qnxnto.c
+++ HEAD_641/pkgsrc/devel/scmgit-base/files/qnxnto.c
@@ -7,41 +7,66 @@
 #include <errno.h>
 #include <unistd.h>
 
-void *
+static void *
 restart_thread(void *arg)
 {
 	for (;;)
 		pause();
 }
 
+static pthread_t
+startit(void)
+{
+	sigset_t	fullset, oset;
+	int		ret;
+	pthread_t	tid;
+
+	/* Start restart thread with all signals blocked */
+	sigfillset(&fullset);
+	pthread_sigmask(SIG_BLOCK, &fullset, &oset);
+
+	if ((ret = pthread_create(&tid, NULL, restart_thread,
+	    NULL)) != EOK) {
+		tid = 0;
+		errno = ret;
+	}
+
+	/* restore */
+	pthread_sigmask(SIG_SETMASK, &oset, NULL);
+
+	return tid;
+}
+
 int
 sigaction_qnx(int sig, const struct sigaction *act, struct sigaction *oact)
 {
 	static pthread_t	restart_tid;
-	sigset_t		nset;
-	int			ret;
+	sigset_t		nset, oset;
 	struct sigaction	act_new;
 
-	if ((act->sa_flags & SA_RESTART) == 0) {
-		return sigaction(sig, act, oact);
-	}
-
-	/* SA_RESTART */
-	if (restart_tid == 0 && (ret = pthread_create(&restart_tid,
-	    NULL, restart_thread, NULL)) != EOK) {
-		errno = ret;
-		return -1;
-	}
-
-	/* Block in calling thread */
 	sigemptyset(&nset);
 	sigaddset(&nset, sig);
-	pthread_sigmask(SIG_BLOCK, &nset, NULL);
 
-	/* Make sure unblocked in restart thread if not already */
-	SignalProcmask_r(0, restart_tid, SIG_UNBLOCK, &nset, NULL);
+	if (act->sa_flags & SA_RESTART) {
+		if (restart_tid == 0 && (restart_tid = startit()) == 0) {
+			return -1;
+		}
 
-	act_new = *act;
-	act_new.sa_flags &= ~SA_RESTART;
-	return sigaction(sig, &act_new, oact);
+		/* Block in calling thread */
+		pthread_sigmask(SIG_BLOCK, &nset, NULL);
+
+		/* Make sure unblocked in restart thread if not already */
+		SignalProcmask_r(0, restart_tid, SIG_UNBLOCK, &nset, NULL);
+
+		act_new = *act;
+		act_new.sa_flags &= ~SA_RESTART;
+		act = &act_new;
+	}
+	else if (restart_tid != 0) {
+		/* Unblock in calling thread */
+		pthread_sigmask(SIG_UNBLOCK, &nset, NULL);
+		/* Block in restart thread */
+		SignalProcmask_r(0, restart_tid, SIG_BLOCK, &nset, NULL);
+	}
+	return sigaction(sig, act, oact);
 }
