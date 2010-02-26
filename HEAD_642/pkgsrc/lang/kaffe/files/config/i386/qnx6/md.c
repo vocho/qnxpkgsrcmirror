b46c56aa8c919cab2f4737131749fec642e62a61
seanb <seanb@qnx.com>
2010-02-26 04:16:08 +0000

    - initial kaffe support. - builds, tests run but most fail.

---
+++ HEAD_642/pkgsrc/lang/kaffe/files/config/i386/qnx6/md.c
@@ -0,0 +1,12 @@
+/*
+ * i386/QNX6/md.c
+ * QNX6 i386 specific functions.
+ *
+ * Copyright (c) 1996, 1997
+ *	Transvirtual Technologies, Inc.  All rights reserved.
+ *
+ * See the file "license.terms" for information on usage and redistribution 
+ * of this file. 
+ */
+
+#include <stdio.h>
