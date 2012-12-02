f62ad52da40122d937c2035672485a528ea90ce6
seanb@qnx.com <seanb@qnx.com@b0d920d6-249f-4265-941a-613bfc6fc4a4>
2012-12-02 17:04:00 +0000

    - clean up.

--- HEAD_650/pkgsrc/security/gnutls/patches/patch-gl_stdio.in.h
+++ HEAD_650/pkgsrc/security/gnutls/patches/patch-gl_stdio.in.h
@@ -2,9 +2,9 @@ $NetBSD: patch-gl_stdio.in.h,v 1.1 2012/08/02 09:37:32 jperkin Exp $
 
 Avoid conflict between gets() and std::gets()
 
---- gl/stdio.in.h.orig	2012-06-05 17:07:53.000000000 +0000
-+++ gl/stdio.in.h
-@@ -699,6 +699,7 @@ _GL_WARN_ON_USE (getline, "getline is un
+--- gl/stdio.in.h.orig	Thu Aug  2 09:29:29 2012
++++ gl/stdio.in.h	Thu Aug  2 09:30:17 2012
+@@ -699,6 +699,7 @@
  # endif
  #endif
  
@@ -12,7 +12,7 @@ Avoid conflict between gets() and std::gets()
  /* It is very rare that the developer ever has full control of stdin,
     so any use of gets warrants an unconditional warning; besides, C11
     removed it.  */
-@@ -706,6 +707,7 @@ _GL_WARN_ON_USE (getline, "getline is un
+@@ -706,6 +707,7 @@
  #if HAVE_RAW_DECL_GETS
  _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
  #endif
